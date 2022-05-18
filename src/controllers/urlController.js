ec4d122406982e2997093d739e505c383eb61608
AmitKumarSharma4046 <aerez4046@gmail.com>
2022-05-18 23:29:55 +0530

    URL-SHORTENER

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,10 +1,11 @@
 const validator = require('validator')
-const shortURL = require('node-url-shortener')
 const redis = require('redis')
 const { promisify } = require("util");
+const shortid = require('shortid')
+
 
 const urlModel = require('../models/urlModel')
-
+const baseUrl = 'https://localhost:3000'
 
 const isValid = function (value) {
     if (typeof value === "undefined" || typeof value === null) return false
@@ -56,23 +57,18 @@ const postUrlShorten = async function (req, res) {
         let alreadyExistingUrl = await urlModel.findOne({ longUrl: longUrl })
         if (alreadyExistingUrl) return res.status(400).send({ status: false, msg: `shortUrl for ${longUrl} exists.` })
 
-        //logUrl:"https://www.npmjs.com/package/node-url-shortener"
-        shortURL.short(longUrl, async function (err, url) {
-            if (err) return res.status(400).send({ status: false, msg: 'Error in shortening of url' })
-            if (url) {
-                shortUrl = url // url:"https://cdpt.in/lv"
-                urlCode = url.slice(url.lastIndexOf('/') + 1)//15+1(lv)
+        urlCode = shortid.generate().toLowerCase()
+        shortUrl = baseUrl + '/' + urlCode
 
-                let creationData = { urlCode, longUrl, shortUrl }
-                let newSortUrl = await urlModel.create(creationData)
-                res.status(201).send({ status: true, data:{
-                    longUrl:newSortUrl.longUrl,
-                    shortUrl:newSortUrl.shortUrl,
-                    urlCode:newSortUrl.urlCode
-                } })
+        let creationData = { urlCode, longUrl, shortUrl }
+        let newSortUrl = await urlModel.create(creationData)
+        res.status(201).send({
+            status: true, data: {
+                longUrl: newSortUrl.longUrl,
+                shortUrl: newSortUrl.shortUrl,
+                urlCode: newSortUrl.urlCode
             }
-        });
-
+        })
     }
     catch (error) {
         res.status(500).send({ status: false, msg: error.message })
@@ -84,14 +80,15 @@ const getUrl = async function (req, res) {
     try {
         urlCode = req.params.urlCode
         let url = await GET_ASYNC(`${urlCode}`)
-        
+
         if (url) {
-             res.redirect(302, url)
+            console.log(url)
+            res.redirect(302, url)
         } else {
             let newURL = await urlModel.findOne({ urlCode: urlCode }).select({ longUrl: 1, _id: 0 })
             if (!newURL) return res.status(404).send({ status: false, msg: 'longUrl not found' })
-            await SET_ASYNC(`${urlCode}`, JSON.stringify(newURL))
-            res.redirect(302, newURL)
+            await SET_ASYNC(`${urlCode}`, JSON.stringify(newURL.longUrl))
+            res.redirect(302, newURL.longUrl)
         }
     }
     catch (error) {
